4e89328388d489a2bf6e9350bbdd0544867234b1
Aviral Goel <goel.aviral@gmail.com>
2020-07-30 13:07:43 -0400

    Refactor IfConditionalExpressionRNode

--- inst/include/IfConditionalExpressionRNode.hpp
+++ inst/include/IfConditionalExpressionRNode.hpp
@@ -2,9 +2,6 @@
 #define RASTR_AST_IF_CONDITIONAL_EXPRESSION_RNODE_HPP
 
 #include "ConditionalExpressionRNode.hpp"
-#include "KeywordRNode.hpp"
-#include "ConditionRNode.hpp"
-#include "ExpressionRNode.hpp"
 
 namespace rastr {
 namespace ast {
@@ -15,26 +12,10 @@ class IfConditionalExpressionRNode: public ConditionalExpressionRNode {
         KeywordRNodeSPtr if_keyword,
         ConditionRNodeSPtr condition,
         ExpressionRNodeSPtr consequent_expression)
-        : ConditionalExpressionRNode(condition)
-        , if_keyword_(if_keyword)
-        , consequent_expression_(consequent_expression) {
+        : ConditionalExpressionRNode(if_keyword,
+                                     condition,
+                                     consequent_expression) {
         set_type(Type::IfConditionalExpressionRNode);
-    }
-
-    KeywordRNodeSPtr get_if_keyword() const {
-        return if_keyword_;
-    }
-
-    void set_if_keyword(KeywordRNodeSPtr if_keyword) {
-        if_keyword_ = if_keyword;
-    }
-
-    ExpressionRNodeSPtr get_consequent_expression() const {
-        return consequent_expression_;
-    }
-
-    void set_consequent_expression(ExpressionRNodeSPtr consequent_expression) {
-        consequent_expression_ = consequent_expression;
     }
 
     static void initialize();
@@ -44,9 +25,6 @@ class IfConditionalExpressionRNode: public ConditionalExpressionRNode {
     static SEXP get_class();
 
   private:
-    KeywordRNodeSPtr if_keyword_;
-    ExpressionRNodeSPtr consequent_expression_;
-
     static SEXP class_;
 }; // namespace ast
 
