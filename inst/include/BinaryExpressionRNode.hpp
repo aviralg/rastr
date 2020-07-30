905a22bf882b357fb98c51a0035629d201fbcffc
Aviral Goel <goel.aviral@gmail.com>
2020-07-30 13:00:21 -0400

    Refactor BinaryExpressionRNode to use IOperator interface

--- inst/include/BinaryExpressionRNode.hpp
+++ inst/include/BinaryExpressionRNode.hpp
@@ -1,33 +1,31 @@
 #ifndef RASTR_AST_BINARY_EXPRESSION_RNODE_HPP
 #define RASTR_AST_BINARY_EXPRESSION_RNODE_HPP
 
-#include "OperatorRNode.hpp"
+#include "IOperator.hpp"
 #include "ExpressionRNode.hpp"
 
 namespace rastr {
 namespace ast {
 
-class BinaryExpressionRNode: public ExpressionRNode {
+class BinaryExpressionRNode
+    : public ExpressionRNode
+    , public IOperator {
   public:
     BinaryExpressionRNode(OperatorRNodeSPtr op,
-                          ExpressionRNodeSPtr left_expression,
-                          ExpressionRNodeSPtr right_expression)
+                          ExpressionRNodeSPtr first_operand,
+                          ExpressionRNodeSPtr second_operand)
         : ExpressionRNode()
-        , op_(op)
-        , left_expression_(left_expression)
-        , right_expression_(right_expression) {
+        , IOperator(op)
+        , first_operand_(first_operand)
+        , second_operand_(second_operand) {
     }
 
-    OperatorRNodeSPtr get_operator() const {
-        return op_;
+    ExpressionRNodeSPtr get_first_operand() const {
+        return first_operand_;
     }
 
-    ExpressionRNodeSPtr get_left_expression() const {
-        return left_expression_;
-    }
-
-    ExpressionRNodeSPtr get_right_expression() const {
-        return right_expression_;
+    ExpressionRNodeSPtr get_second_operand() const {
+        return second_operand_;
     }
 
     static void initialize();
@@ -37,9 +35,8 @@ class BinaryExpressionRNode: public ExpressionRNode {
     static SEXP get_class();
 
   private:
-    OperatorRNodeSPtr op_;
-    ExpressionRNodeSPtr left_expression_;
-    ExpressionRNodeSPtr right_expression_;
+    ExpressionRNodeSPtr first_operand_;
+    ExpressionRNodeSPtr second_operand_;
 
     static SEXP class_;
 };
