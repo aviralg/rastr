9c596bf22938bcc1c7fae2fd8ceaacf0676c2e85
Aviral Goel <goel.aviral@gmail.com>
2020-07-30 13:05:28 -0400

    Refactor ConditionalExpressionRNode to use ICondition interface

--- inst/include/ConditionalExpressionRNode.hpp
+++ inst/include/ConditionalExpressionRNode.hpp
@@ -1,30 +1,48 @@
 #ifndef RASTR_AST_CONDITIONAL_EXPRESSION_RNODE_HPP
 #define RASTR_AST_CONDITIONAL_EXPRESSION_RNODE_HPP
 
-#include "ConditionRNode.hpp"
+#include "ICondition.hpp"
 #include "ExpressionRNode.hpp"
+#include "KeywordRNode.hpp"
 
 namespace rastr {
 namespace ast {
 
-class ConditionalExpressionRNode: public ExpressionRNode {
+class ConditionalExpressionRNode
+    : public ExpressionRNode
+    , public ICondition {
   public:
-    explicit ConditionalExpressionRNode(ConditionRNodeSPtr condition)
-        : ExpressionRNode(), condition_(condition) {
+    explicit ConditionalExpressionRNode(
+        KeywordRNodeSPtr if_keyword,
+        ConditionRNodeSPtr condition,
+        ExpressionRNodeSPtr consequent_expression)
+        : ExpressionRNode()
+        , ICondition(condition)
+        , if_keyword_(if_keyword)
+        , consequent_expression_(consequent_expression) {
     }
 
-    ConditionRNodeSPtr get_condition() const {
-        return condition_;
+    virtual ~ConditionalExpressionRNode() = default;
+
+    KeywordRNodeSPtr get_if_keyword() const {
+        return if_keyword_;
     }
 
-    void set_condition(ConditionRNodeSPtr condition) {
-        condition_ = condition;
+    void set_if_keyword(KeywordRNodeSPtr if_keyword) {
+        if_keyword_ = if_keyword;
+    }
+
+    ExpressionRNodeSPtr get_consequent_expression() const {
+        return consequent_expression_;
+    }
+
+    void set_consequent_expression(ExpressionRNodeSPtr consequent_expression) {
+        consequent_expression_ = consequent_expression;
     }
 
   private:
-    ConditionRNodeSPtr condition_;
-
-    static SEXP class_;
+    KeywordRNodeSPtr if_keyword_;
+    ExpressionRNodeSPtr consequent_expression_;
 }; // namespace ast
 
 using ConditionalExpressionRNodeSPtr =
