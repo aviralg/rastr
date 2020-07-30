afa81008776bba096cc4a240af288e66951021e8
Aviral Goel <goel.aviral@gmail.com>
2020-07-29 22:00:44 -0400

    Refactor BoundedRNode to Bounded interface

--- inst/include/Bounded.hpp
+++ inst/include/Bounded.hpp
@@ -1,22 +1,23 @@
-#ifndef RASTR_AST_BOUNDED_RNODE_HPP
-#define RASTR_AST_BOUNDED_RNODE_HPP
+#ifndef RASTR_AST_BOUNDED_HPP
+#define RASTR_AST_BOUNDED_HPP
 
-#include "RNode.hpp"
 #include "DelimiterRNode.hpp"
 
 namespace rastr {
 namespace ast {
 
-class BoundedRNode: public virtual RNode {
+class Bounded {
   public:
-    explicit BoundedRNode(): RNode() {
+    explicit Bounded() {
     }
 
-    explicit BoundedRNode(DelimiterRNodeSPtr opening_delimiter,
-                          DelimiterRNodeSPtr closing_delimiter)
-        : BoundedRNode() {
+    explicit Bounded(DelimiterRNodeSPtr opening_delimiter,
+                     DelimiterRNodeSPtr closing_delimiter)
+        : Bounded() {
         set_delimiters(opening_delimiter, closing_delimiter);
     }
+
+    virtual ~Bounded() = default;
 
     DelimiterRNodeSPtr get_opening_delimiter() const {
         return opening_delimiter_;
@@ -40,22 +41,12 @@ class BoundedRNode: public virtual RNode {
         set_closing_delimiter(closing_delimiter);
     }
 
-    static void initialize();
-
-    static void finalize();
-
-    static SEXP get_class();
-
   private:
     DelimiterRNodeSPtr opening_delimiter_;
     DelimiterRNodeSPtr closing_delimiter_;
-
-    static SEXP class_;
 };
-
-using BoundedRNodeSPtr = std::shared_ptr<BoundedRNode>;
 
 } // namespace ast
 } // namespace rastr
 
-#endif /* RASTR_AST_BOUNDED_RNODE_HPP */
+#endif /* RASTR_AST_BOUNDED_HPP */
